74029497cd41d40e239c1e80d2f54def7016139b
Arjan van der Gaag <arjan@Kabisa.nl>
2013-02-26 18:55:25 +0100

    Replace placeholder with key value

--- templater.rb
+++ templater.rb
@@ -2,25 +2,46 @@ require 'rubygems'
 require 'rspec/autorun'
 
 class Templater
-  attr_reader :template
+  attr_reader :template, :data
 
-  def initialize(template)
-    @template = template
+  def initialize(template, data = {})
+    @template, @data = template, data
   end
 
   def render
-    template
+    template.sub(/\{\{([^}]+)\}\}/) do
+      data.fetch($1.to_sym, '')
+    end
   end
 end
 
 describe Templater do
-  subject { described_class.new(template).render }
+  subject    { described_class.new(template, data).render }
+  let(:data) { { } }
 
   context 'with a regular string' do
     let(:template) { 'Hello, world!' }
 
     it 'returns the string unaltered' do
       should == 'Hello, world!'
+    end
+  end
+
+  context 'with a single placeholder' do
+    let(:template) { 'Hello, {{name}}!' }
+
+    context 'when the key is present' do
+      let(:data) { { name: 'Eric' } }
+
+      it 'replaces the placeholder with they key value' do
+        should == 'Hello, Eric!'
+      end
+    end
+
+    context 'when the key is not present' do
+      it 'omits the placeholder' do
+        should == 'Hello, !'
+      end
     end
   end
 end
