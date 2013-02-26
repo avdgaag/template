78f6be429ce23d6616de19e12e54dc1df5feb7aa
Arjan van der Gaag <arjan@Kabisa.nl>
2013-02-26 18:50:34 +0100

    Return placeholder-less templates as is

--- templater.rb
+++ templater.rb
@@ -1,5 +1,26 @@
 require 'rubygems'
 require 'rspec/autorun'
 
+class Templater
+  attr_reader :template
+
+  def initialize(template)
+    @template = template
+  end
+
+  def render
+    template
+  end
+end
+
 describe Templater do
+  subject { described_class.new(template).render }
+
+  context 'with a regular string' do
+    let(:template) { 'Hello, world!' }
+
+    it 'returns the string unaltered' do
+      should == 'Hello, world!'
+    end
+  end
 end
